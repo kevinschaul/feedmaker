08f45f7a696bf028644ee76c04419c139d5f526c
Kevin Schaul <kevin.schaul@gmail.com>
2024-01-05 08:48:15 -0600

    Update production.py

--- feedmaker/settings/production.py
+++ feedmaker/settings/production.py
@@ -7,6 +7,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 MIDDLEWARE += [
     "django.middleware.cache.UpdateCacheMiddleware",
     "django.middleware.common.CommonMiddleware",
